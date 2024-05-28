dcef52141f00370ce61aeeecd365c5db00e218f4
Phạm Đức Hào <hao.pham@Haos-MacBook-Pro.local>
2024-05-28 17:13:41 +0700

    Run successfully network with 3 peers

--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,7 @@
 ./prysmctl testnet generate-genesis \
  --fork capella \
  --num-validators 64 \
- --genesis-time-delay 600 \
+ --genesis-time-delay 30 \
  --chain-config-file ./consensus/config.yml \
  --geth-genesis-json-in ./execution/genesis.json  \
  --geth-genesis-json-out ./execution/genesis.json \
